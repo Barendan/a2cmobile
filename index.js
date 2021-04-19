2d8fe8862f165d62c8fe175ed6d0356aea778315
Justin Calvo <justin_calvo@hotmail.com>
2021-04-19 16:43:09 -0400

    Modifying for merging into sprint 1 branch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
@@ -8,14 +8,14 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
-function HeadlessCheck({isHeadless}) {
+const HeadlessCheck = ({isHeadless}) => {
   if (isHeadless) {
     // App has been launched in the background by iOS, ignore
     return null;
   }
 
   return <App />;
-}
+};
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck);
 AppRegistry.registerHeadlessTask(
