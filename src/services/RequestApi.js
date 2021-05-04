029af8145e3bd0ee90aa91241ee9399b84aa8d17
Daniel Barenboim <Daniel.Barenboim@gmr.net>
2021-05-04 15:57:15 -0400

    use interceptors on req/res for error handling and lang locale

--- src/services/RequestApi.js
+++ src/services/RequestApi.js
@@ -12,4 +12,28 @@ const RequestApi = axios.create({
   },
 });
 
+RequestApi.interceptors.request.use(
+  config => {
+    const selectedLanguage = localStorage.getItem('i18nextLng') || 'en';
+    config.headers['X-App-LAnguage'] = `${selectedLanguage}`;
+
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers['Authorization'] = `Token ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error),
+);
+
+RequestApi.interceptors.response.use(
+  response => {
+    return response.data.result;
+  },
+  error => {
+    return Promise.reject(error.response.data);
+  },
+);
+
 export default RequestApi;
