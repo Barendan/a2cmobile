5076177d2fce710d0bf5499a0b6a36acce67310b
John Bridges <john.bridges@gmr.net>
2021-05-05 10:16:23 -0700

    Remove src/A2CMobile.Api, src/A2CMobile.Database, and test/A2CMobile.Api.Test folders and files (moved to "a2cmobileapi" repo)

--- src/A2CMobile.Api/Constants/HttpContentMediaTypes.cs
+++
@@ -1,8 +0,0 @@
-
-namespace A2CMobile.Api.Constants
-{
-    internal class HttpContentMediaTypes
-    {
-        public const string JSON = "application/json";
-    }
-}
--- src/A2CMobile.Api/Infrastructure/Helpers/PropertyValidation.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace A2CMobile.Api.Infrastructure.Helpers
-{
-    public static class PropertyValidation
-    {
-        public static bool IsValidDateTime(DateTime date) => date == default ? false : true;
-    }
-}
