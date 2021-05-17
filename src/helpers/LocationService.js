8c23604b2a6c9daa31b1cb70a2144f211c2091d8
Daniel Barenboim <Daniel.Barenboim@gmr.net>
2021-05-16 21:12:33 -0400

    create google places autocomplete component

--- src/helpers/LocationService.js
+++ src/helpers/LocationService.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Platform, PermissionsAndroid } from 'react-native';
 import Geocoder from 'react-native-geocoder';
 import Geolocation from 'react-native-geolocation-service';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-Geocoder.init('AIzaSyC5xgnxq3omtAwH5UenHoL_mmvsfP7mU5A');
+// test key; replace with prod key
+const google_api_key = 'AIzaSyC5xgnxq3omtAwH5UenHoL_mmvsfP7mU5A';
+
+Geocoder.init(google_api_key);
 // Geocoder.init("xxxxxx", {language : "en"}); // can set the language
 
 const requestLocationPermission = async () => {
@@ -73,8 +77,43 @@ const geocodeLocationByCoords = (lat, long) =>
       .catch(error => reject(error));
   });
 
+const googlePlacesAutoInput = ({ lang }) => (
+  <GooglePlacesAutocomplete
+    placeholder="Enter Location"
+    minLength={2}
+    autoFocus={false}
+    returnKeyType={'default'}
+    fetchDetails={true}
+    onPress={(data, details = null) => {
+      // 'details' are provided when fetchDetails = true
+      console.log(data, details);
+    }}
+    onFail={error => console.error(error)}
+    query={{
+      key: google_api_key,
+      language: lang,
+    }}
+    styles={{
+      textInputContainer: {
+        backgroundColor: 'grey',
+        width: 300,
+        height: 30,
+      },
+      textInput: {
+        height: 38,
+        color: '#5d5d5d',
+        fontSize: 16,
+      },
+      predefinedPlacesDescription: {
+        color: '#1faadb',
+      },
+    }}
+  />
+);
+
 export default {
   getLocation,
   geocodeLocationByName,
   geocodeLocationByCoords,
+  googlePlacesAutoInput,
 };
