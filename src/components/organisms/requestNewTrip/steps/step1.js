2a0013d935378d8cdd380014e229ed71fbd82e28
christian morales <christianmorales@gmr.net>
2021-05-27 12:24:16 -0500

    refactored requestNewTrip to steps

---
+++ src/components/organisms/requestNewTrip/steps/step1.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import {
+  LocationSearchCard,
+  CheckboxCard,
+  NumericCountCard,
+  DateTimePickerCard,
+  DropDownPickerCard,
+  TextInputCard,
+} from '_organisms';
+import { Inset, Stack } from 'react-native-spacing-system';
+import styles from './styles';
+import { useTripDetails } from './useTripDetails';
+
+const step1 = ({ back, next, saveState }) => {
+  const { tripDetails, onRoundTripChecked } = useTripDetails();
+  const { t } = useTranslation();
+
+  const nextStep = () => {
+    // Save state for use in other steps
+    next();
+  };
+
+  const goBack = () => {
+    back();
+  };
+
+  return (
+    <View>
+      <Text>Step 1</Text>
+
+      <LocationSearchCard
+        locationIndex={0}
+        title={'Pickup Address'}
+        description={
+          tripDetails.pickupAddress
+            ? tripDetails.pickupAddress.FormattedAddress
+            : 'This is the location where you will be picked up from'
+        }
+        onAddressSelected={addr => onAddressSelected('pickupAddress', addr)}
+      />
+
+      {tripDetails.tripStops.map((currentStop, index) => (
+        <>
+          <LocationSearchCard
+            disableClick={true}
+            icon={'minus'}
+            iconColor={GRAY_BLUE}
+            showBorder={true}
+            title={'Trip Stop ' + (index + 1)}
+            description={currentStop.FormattedAddress}
+            onPress={() => deleteTripStop(index)}
+          />
+        </>
+      ))}
+
+      <LocationSearchCard
+        showBorder={true}
+        dashedBorder={true}
+        title={'Add A Stop (Optional)'}
+        description={'Add a stop address on your way to your destination'}
+        onAddressSelected={addr => onAddressSelected('tripStops', addr)}
+      />
+
+      <LocationSearchCard
+        locationIndex={1}
+        title={'Destination Address'}
+        description={
+          tripDetails.destinationAddress
+            ? tripDetails.destinationAddress.FormattedAddress
+            : 'This is the location where you will be dropped off'
+        }
+        onAddressSelected={addr =>
+          onAddressSelected('destinationAddress', addr)
+        }
+      />
+      <CheckboxCard
+        cardIcon={'swap-horizontal'}
+        title={'Is this a round trip?'}
+        checkedValue={tripDetails.isRoundTrip}
+        onChecked={value => onRoundTripChecked(value)}
+      />
+      <View style={styles.callToActionBtnHolder}>
+        <Inset all={16}>
+          <TouchableOpacity style={styles.customBtnBG} onPress={next}>
+            <Text style={styles.customBtnText}>{t('Continue')}</Text>
+          </TouchableOpacity>
+        </Inset>
+      </View>
+    </View>
+  );
+};
+
+export default step1;
---
+++ src/components/organisms/requestNewTrip/steps/useTripDetails.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+export const useTripDetails = () => {
+  const { user } = useSelector(state => state.user);
+  //Trip Details
+  const [tripDetails, setTripDetails] = React.useState({
+    pickupAddress: null,
+    tripStops: [],
+    destinationAddress: null,
+    isRoundTrip: false,
+    additionalPassengers: 0,
+    wheelchairRequired: false,
+    appointmentDateTime: null,
+    appointmentDate: '',
+    appointmentTime: '',
+    tripReason: '',
+    specialNeeds: '',
+  });
+
+  const updateTripDetails = React.useCallback((key, value) => {
+    setTripDetails(tripDetails => {
+      return {
+        ...tripDetails,
+        [key]: value,
+      };
+    });
+  }, []);
+
+  const onAddressSelected = (type, value) => {
+    if (type.toLowerCase() === 'tripstops') {
+      updateTripDetails('tripStops', [...tripDetails.tripStops, value]);
+    } else {
+      updateTripDetails(type, value);
+    }
+  };
+
+  const deleteTripStop = index => {
+    let newTripStops = tripDetails.tripStops;
+    updateTripDetails('tripStops', newTripStops);
+  };
+
+  const onRoundTripChecked = value => {
+    updateTripDetails('isRoundTrip', value);
+  };
+
+  const onAdditionPassengersChange = value => {
+    updateTripDetails('additionalPassengers', value);
+  };
+
+  const onWheelchairRequiredChecked = value => {
+    updateTripDetails('wheelchairRequired', value);
+  };
+
+  const onAppointmentDateTimeChange = (type, value) => {
+    switch (type) {
+      case 'date':
+        updateTripDetails('appointmentDate', value);
+        break;
+      case 'time':
+        updateTripDetails('appointmentTime', value);
+        break;
+      case 'datetime':
+        updateTripDetails('appointmentDateTime', value);
+        break;
+      default:
+        console.log('Unknown type. Only Date and Time Required');
+    }
+  };
+
+  const onTripReasonSelected = selected => {
+    updateTripDetails('tripReason', selected.value);
+  };
+
+  const onSpecialNeedsEntered = value => {
+    updateTripDetails('specialNeeds', value);
+  };
+
+  const onRequestTrip = () => {
+    var payload = {
+      memberID: user.memberID,
+      NETMember_ID: user.NETMember_ID,
+      pickupAddress: tripDetails.pickupAddress.FormattedAddress,
+      pickupAddressLat: tripDetails.pickupAddress.Latitude,
+      pickupAddressLng: tripDetails.pickupAddress.Longitude,
+      pickupAddress1: tripDetails.pickupAddress.AddressLine1,
+      pickupAddress2: tripDetails.pickupAddress.AddressLine2,
+      pickupAddressCity: tripDetails.pickupAddress.City,
+      pickupAddressState: tripDetails.pickupAddress.State,
+      pickupAddressCounty: tripDetails.pickupAddress.County,
+      pickupAddressZip: tripDetails.pickupAddress.ZipCode,
+      pickupAddressCountry: tripDetails.pickupAddress.Country,
+      destinationAddress: tripDetails.destinationAddress.FormattedAddress,
+      destinationAddressLat: tripDetails.destinationAddress.Latitude,
+      destinationAddressLng: tripDetails.destinationAddress.Longitude,
+      destinationAddress1: tripDetails.destinationAddress.AddressLine1,
+      destinationAddress2: tripDetails.destinationAddress.AddressLine2,
+      destinationAddressCity: tripDetails.destinationAddress.City,
+      destinationAddressState: tripDetails.destinationAddress.State,
+      destinationAddressCounty: tripDetails.destinationAddress.County,
+      destinationAddressZip: tripDetails.destinationAddress.ZipCode,
+      destinationAddressCountry: tripDetails.destinationAddress.Country,
+      tripStops: tripDetails.tripStops,
+      isRoundTrip: tripDetails.isRoundTrip,
+      additionalPassengers: tripDetails.additionalPassengers,
+      wheelchair: tripDetails.wheelchairRequired ? 'yes' : 'no',
+      appointmentDateTime: tripDetails.appointmentDateTime,
+      appointmentDate: tripDetails.appointmentDate,
+      appointmentTime: tripDetails.appointmentTime,
+      tripReason: tripDetails.tripReason,
+      specialNeeds: tripDetails.specialNeeds,
+    };
+
+    console.log(JSON.stringify(payload));
+
+    setLoading(true);
+    MemberService.requestTrip(payload)
+      .then(data => {
+        setLoading(false);
+        alert(JSON.stringify(data));
+      })
+      .catch(err => {
+        alert(JSON.stringify(err));
+
+        setLoading(false);
+      });
+  };
+
+  return {
+    tripDetails,
+    setTripDetails,
+    updateTripDetails,
+    onAddressSelected,
+    deleteTripStop,
+    onRoundTripChecked,
+    onAdditionPassengersChange,
+    onWheelchairRequiredChecked,
+    onAppointmentDateTimeChange,
+    onTripReasonSelected,
+    onSpecialNeedsEntered,
+    onRequestTrip,
+  };
+};
