057e05009abaab16d7b684485cc04b792a41db2a
Daniel Barenboim <Daniel.Barenboim@gmr.net>
2021-06-04 12:38:05 -0400

    incorporate components and styling into step1

--- src/scenes/savedLocations/locationSteps/step1.js
+++ src/scenes/savedLocations/locationSteps/step1.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { GooglePlacesInput } from '_helpers/LocationService';
+import { IconButton } from 'react-native-paper';
 
 const Step1 = ({ back, next, saveState }) => {
-  return <View></View>;
+  const nextStep = data => {
+    const addressMainText = data.structured_formatting.main_text;
+    saveState({ address: addressMainText });
+    next();
+  };
+
+  return (
+    <View style={styles.searchBarContainer}>
+      <IconButton
+        icon="arrow-left"
+        color="#000"
+        size={26}
+        onPress={back}
+        style={{ marginTop: 10, marginRight: 2 }}
+      />
+      <GooglePlacesInput handleSubmit={nextStep} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  searchBarContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    paddingBottom: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 10,
+  },
+});
 
 export default Step1;
