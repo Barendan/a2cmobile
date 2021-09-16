7a604e5221659389ea9dc8f9da8aa3f40e62d4e2
Daniel Barenboim <Daniel.Barenboim@gmr.net>
2021-09-16 11:19:50 -0400

    adding in biometrics, changing styling, and fixing errors

--- src/store/plan.js
+++ src/store/plan.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import  storage  from './../storage';
+import storage from './../storage';
 
 // Slice
 // const initialPlan = localStorage.getItem('plan')
@@ -13,26 +13,26 @@ let initialMemberPlans = [];
 storage
   .load({
     key: 'plan',
- 
+
     // autoSync (default: true) means if data is not found or has expired,
     // then invoke the corresponding sync method
     autoSync: true,
- 
+
     // syncInBackground (default: true) means if data expired,
     // return the outdated data first while invoking the sync method.
     // If syncInBackground is set to false, and there is expired data,
     // it will wait for the new data and return only after the sync completed.
     // (This, of course, is slower)
     syncInBackground: true,
- 
+
     // you can pass extra params to the sync method
     // see sync example below
     syncParams: {
       extraFetchOptions: {
         // blahblah
       },
-      someFlag: true
-    }
+      someFlag: true,
+    },
   })
   .then(ret => {
     // found data go to then()
@@ -41,40 +41,40 @@ storage
   .catch(err => {
     // any exception including data not found
     // goes to catch()
-    console.warn(err.message);
+    // console.warn(err.message);
     switch (err.name) {
       case 'NotFoundError':
-        // TODO;
+        console.log('No user found.');
         break;
       case 'ExpiredError':
-        // TODO
+        console.log('User expired.');
         break;
     }
   });
 
-  storage
+storage
   .load({
     key: 'memberPlans',
- 
+
     // autoSync (default: true) means if data is not found or has expired,
     // then invoke the corresponding sync method
     autoSync: true,
- 
+
     // syncInBackground (default: true) means if data expired,
     // return the outdated data first while invoking the sync method.
     // If syncInBackground is set to false, and there is expired data,
     // it will wait for the new data and return only after the sync completed.
     // (This, of course, is slower)
     syncInBackground: true,
- 
+
     // you can pass extra params to the sync method
     // see sync example below
     syncParams: {
       extraFetchOptions: {
         // blahblah
       },
-      someFlag: true
-    }
+      someFlag: true,
+    },
   })
   .then(ret => {
     // found data go to then()
@@ -83,61 +83,88 @@ storage
   .catch(err => {
     // any exception including data not found
     // goes to catch()
-    console.warn(err.message);
+    // console.warn(err.message);
     switch (err.name) {
       case 'NotFoundError':
-        // TODO;
+        console.log('No user found.');
         break;
       case 'ExpiredError':
-        // TODO
+        console.log('User expired.');
         break;
     }
   });
-
 
 const slice = createSlice({
   name: 'plan',
   initialState: {
     plan: initialPlan,
-    memberPlans: initialMemberPlans
+    memberPlans: initialMemberPlans,
   },
   reducers: {
     updateCurrentPlan: (state, action) => {
       state.plan = action.payload;
-      storage.save({
-        key: 'plan', // Note: Do not use underscore("_") in key!
-        id: 'currentPlan', // Note: Do not use underscore("_") in id!
-        data: action.payload,
-        expires: null
-      });
+      storage
+        .save({
+          key: 'plan', // Note: Do not use underscore("_") in key!
+          id: 'currentPlan', // Note: Do not use underscore("_") in id!
+          data: action.payload,
+          expires: null,
+        })
+        .catch(err => {
+          // any exception including data not found
+          // goes to catch()
+          // console.warn(err.message);
+          switch (err.name) {
+            case 'NotFoundError':
+              console.log('No user to load.');
+              break;
+            case 'ExpiredError':
+              console.log('Data expired.');
+              break;
+          }
+        });
     },
     setCurrentMemberPlans: (state, action) => {
       state.memberPlans = action.payload;
-      storage.save({
-        key: 'memberPlans', // Note: Do not use underscore("_") in key!
-        id: 'memberPlans', // Note: Do not use underscore("_") in id!
-        data: action.payload,
-        expires: null
-      });
-    },      
+      storage
+        .save({
+          key: 'memberPlans', // Note: Do not use underscore("_") in key!
+          id: 'memberPlans', // Note: Do not use underscore("_") in id!
+          data: action.payload,
+          expires: null,
+        })
+        .catch(err => {
+          // any exception including data not found
+          // goes to catch()
+          // console.warn(err.message);
+          switch (err.name) {
+            case 'NotFoundError':
+              console.log('No user to load.');
+              break;
+            case 'ExpiredError':
+              console.log('Data expired.');
+              break;
+          }
+        });
+    },
   },
 });
 export default slice.reducer;
 // Actions
 const { updateCurrentPlan, setCurrentMemberPlans } = slice.actions;
 
-export const updatePlan = (currentPlan) => async dispatch => {
+export const updatePlan = currentPlan => async dispatch => {
   try {
     dispatch(updateCurrentPlan(currentPlan));
   } catch (e) {
     return console.error(e.message);
   }
-}
+};
 
-export const setMemberPlans = (currentPlan) => async dispatch => {
+export const setMemberPlans = currentPlan => async dispatch => {
   try {
     dispatch(setCurrentMemberPlans(currentPlan));
   } catch (e) {
     return console.error(e.message);
   }
-}
+};
