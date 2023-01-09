73eeeeb67b491d9970cc45ecbe7461156666b8fd
Daniel <danielbarenboim@emsc.root01.org>
2023-01-10 10:28:50 +1300

    update app version

--- src/utils/AppSettings.js
+++ src/utils/AppSettings.js
@@ -1,6 +1,6 @@
 const appKey = 'A2C-EHNCASG-jh2@19';
-const appVersion = 'v2.5';
-const buildVersion = "0010";
+const appVersion = 'v2.41';
+const buildVersion = "0020";
 
 export default {
     appKey,
